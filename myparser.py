dc449f0036a9b55a3966cb1a5a0bb0491a180dea
allenwhale <allenwhale@yahoo.com.tw>
2015-03-21 01:20:03 +0800

    update

--- myparser.py
+++ myparser.py
@@ -7,6 +7,16 @@ class ParserHandler:
                 'b',
                 'start',
                 'select']
+    MAP = {
+        'up': 'up',
+        'down': 'down',
+        'right': 'right',
+        'left': 'left',
+        'start': 'start',
+        'select': 'select',
+        'a': 'z',
+        'b': 'x'
+    }
     def __init__(self):
         pass
 
@@ -16,7 +26,7 @@ class ParserHandler:
             found = False
             for a in self.AVAILABLE:
                 if message.find(a) == 0:
-                    ret.append(a)
+                    ret.append(self.MAP[a])
                     message = message[len(a):]
                     found = True
             if not found:
